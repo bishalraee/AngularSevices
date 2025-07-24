57b28844fe5f7c8c9f2c4e694323f95eb64affaf
Bishal Rai <cdiswizard22@gmail.com>
2025-07-24 11:49:04 +0545

    Codes are added

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Master } from './services/master';
+import { inject } from '@angular/core';
+import { HttpClientModule} 
+
+//go through grok and see it once here the problem is with not able to import HttpClientModule
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+    standalone: true,
+    styleUrl: './app.scss',
 })
-export class App {
+export class App implements OnInit {
+  posts: any[] = [];
+  masterService = inject(Master);
   protected title = 'AngularServices-app';
+
+  ngOnInit(): void{
+    this.masterService.getPosts().subscribe(data => {
+      this.posts = data.posts;
+    });
+  }
 }
--- src/app/services/master.ts
+++ src/app/services/master.ts
@@ -1,8 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root' 
 })
 export class Master {
+  //private variable means only accessible within the Master class
+  private apiURL = 'https://dummyjson.com/posts';
+
+  constructor(private httpClient: HttpClient){
+
+  }
   
+  getPosts(): Observable<any>{
+    return this.httpClient.get<any>(this.apiURL);
+  }
+
 }
